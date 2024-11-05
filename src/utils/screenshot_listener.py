17f3fb96597e947877c8466f0bf8fd995f60085e
binqbit <128422584+binqbit@users.noreply.github.com>
2024-11-05 05:07:58 +0200

    add error processing

--- src/utils/screenshot_listener.py
+++ src/utils/screenshot_listener.py
@@ -6,16 +6,19 @@ import time
 last_screenshot = None
 
 def get_screenshot():
-    image = ImageGrab.grabclipboard()
-    if type(image) == PngImagePlugin.PngImageFile:
-        image_bytes = io.BytesIO()
-        image.save(image_bytes, format="PNG")
-        image_bytes = image_bytes.getvalue()
-        global last_screenshot
-        if last_screenshot is None or last_screenshot != image_bytes:
-            last_screenshot = image_bytes
-            return image_bytes
-        return None
+    try:
+        image = ImageGrab.grabclipboard()
+        if type(image) == PngImagePlugin.PngImageFile:
+            image_bytes = io.BytesIO()
+            image.save(image_bytes, format="PNG")
+            image_bytes = image_bytes.getvalue()
+            global last_screenshot
+            if last_screenshot is None or last_screenshot != image_bytes:
+                last_screenshot = image_bytes
+                return image_bytes
+            return None
+    except Exception as e:
+        print(f"Error: {e}")
     return None
 
 def to_base64_url(image_bytes):
